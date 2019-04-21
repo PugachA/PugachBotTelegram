291bf395a7dbf4aa7704b03d20f483d03d05d484
Александр Пугач <fotone97@yandex.ru>
2019-04-21 20:51:33 +0300

    Добавлены шаблоны для принципов ООП построение приложения

---
+++ PugachBotTelegram/Message/StartType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PugachBotTelegram.Message
+{
+    public class StartType : TextMessageType
+    {
+        public StartType(string message)
+        {
+            Message = message;
+        }
+
+        public override IMessageType MessageType { get; set; }
+        public override string Message { get; set; }
+
+        public override string Answer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PugachBotTelegram/Message/TextMessageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PugachBotTelegram.Message
+{
+    abstract public class TextMessageType : IMessageType
+    {
+
+        abstract public string Message { get; set; }
+
+        abstract public IMessageType MessageType { get; set; }
+
+        abstract public string Answer();
+    }
+}
